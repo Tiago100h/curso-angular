e4a36ceab1908a475e927480d164d58a03da356e
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2020-03-21 14:27:33 -0300

    #07 Introdução aos Serviços e Injeção de Dependência (DI)

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   ],
   exports: [
     CursosComponent
-  ]
+  ],
+  providers: [ CursosService ]
 })
 export class CursosModule { }
