ea4d1c43d278acac4ae151982debf4283d680a8c
Tiago100h <tiagogiuliatte.p@gmail.com>
2020-05-07 17:58:37 -0300

    #037 Introdução a Serviços Correção do nome do projeto

--- servicos/src/app/app.component.spec.ts
+++ servicos/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sevicos'`, () => {
+  it(`should have as title 'servicos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sevicos');
+    expect(app.title).toEqual('servicos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sevicos app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('servicos app is running!');
   });
 });
