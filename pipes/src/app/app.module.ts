1b33a9de31f9dce6604503a6888f01c01e28680d
Tiago100h <tiagogiuliatte.p@gmail.com>
2020-05-26 18:31:41 -0300

    #045 #045 Aplicando Locale (internacionalização) nos Pipes

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import br from '@angular/common/locales/pt';
+
+registerLocaleData(br, 'pt-BR');
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
